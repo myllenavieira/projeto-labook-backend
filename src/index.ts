570147051b4423d930130506b115af1dd150e028
myllenavieira <vieirasilvamyllena@gmail.com>
2023-06-05 16:31:51 -0300

    :bricks: ci: Add dos arquivos necessários

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,19 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import { userRouter } from "./router/userRouter";
-import { postRouter } from "./router/postRouter";
-import { likeRouter } from "./router/likeRouter";
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { userRouter } from './router/userRouter'
+import { postRouter } from './router/postRouter'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.use(cors());
+app.listen(Number(process.env.PORT), () => {
+    console.log(`Servidor rodando na porta ${Number(process.env.PORT)}`)
+})
 
-app.listen(Number(process.env.PORT) || 3003, () => {
-    console.log(`Server is running on port ${Number(process.env.PORT) || 3003}`);
-});
-
-app.use("/users", userRouter);
-app.use("/posts", postRouter);
-app.use("/likes", likeRouter);
+app.use("/users", userRouter)
+app.use("/posts", postRouter)
